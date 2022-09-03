13b09dc71f78fc6b2d0445355a15cb4ece13d452
HunterM <zakaspro@gmail.com>
2022-09-03 23:46:30 +0300

    Коммит 5 программа

---
+++ Les5/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите имя пользователя");
+string username = Console.ReadLine();
+if (username.ToLower() == "masha")
+{
+    Console.WriteLine ("Ура это Masha");
+}
+else
+{
+    Console.WriteLine("Привет, ");
+    Console.WriteLine(username);
+}
