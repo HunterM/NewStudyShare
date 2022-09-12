7d8259829fb92cbe8950b503e8e0a3695921f6fe
HunterM <zakaspro@gmail.com>
2022-09-12 19:38:57 +0300

    коммит ДЗ 4_1

--- HWC#4/Program.cs
+++ HWC#4/Program.cs
@@ -3,8 +3,6 @@
 // 3, 5 -> 243 (3⁵)
 // 2, 4 -> 16
 
-//string line;
-//short A = 0;
 Console.WriteLine("Введите число A");
 int A = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine();
@@ -20,6 +18,7 @@ for (int i = 0; i <= B; i++)
     Console.WriteLine("A в степени B = " + Math.Pow(A, i));
 }
 Console.WriteLine();
+
 
 // Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
 // 452 -> 11
@@ -47,6 +46,7 @@ int SumNumber(int numberNew)
 int sumNumber = SumNumber(numberNew);
 Console.WriteLine();
 Console.WriteLine("Сумма цифр в числе: " + sumNumber);
+Console.WriteLine();
 
 // Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
 // 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
